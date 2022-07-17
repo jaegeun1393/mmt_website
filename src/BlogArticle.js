b9e7bccfc17bed60701199763ad987fa4978d8d4
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2022-07-17 00:32:08 -0700

    Update BlogArticle.js

--- src/BlogArticle.js
+++ src/BlogArticle.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { CKEditor, CKEditorContext } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import Context from '@ckeditor/ckeditor5-core/src/context';
 
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -18,8 +20,17 @@ class BlogArticle extends Component {
   render() {
   return (
   <div className="App">
-      
-
+      <CKEditorContext context={ Context }>
+      <h2>Using the CKeditor 5 context feature in React</h2>
+      <CKEditor
+                        editor={ ClassicEditor }
+                        data="<p>Hello from the first editor working with the context!</p>"
+                        onReady={ editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log( 'Editor1 is ready to use!', editor );
+                        } }
+                    />
+      </CKEditorContext>
   </div>
   );
   }
